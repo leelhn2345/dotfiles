e482f74982dded12d7ccac9aa75ddb01a1b83bf2
Nelson Lee <leelhn2345@outlook.com>
2024-02-19 18:32:11 +0000

    fix: missing tmux dir

--- scripts/tmux.sh
+++ scripts/tmux.sh
@@ -6,5 +6,6 @@ TMUX_DIR="$HOME/.config/tmux"
 # installs tmux package manager
 #######################################
 tmux_tpm() {
+	mkdir -p "$TMUX_DIR"
 	git clone https://github.com/tmux-plugins/tpm "$TMUX_DIR/tpm"
 }
