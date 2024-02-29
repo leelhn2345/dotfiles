c03e70ae57e749c666f150e08be6b29a01168833
Nelson Lee <64735315+leelhn2345@users.noreply.github.com>
2024-03-01 02:18:17 +0800

    feat: Update .wezterm.lua

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -6,6 +6,21 @@ if wezterm.config_builder then
 	config = wezterm.config_builder()
 end
 
+-- core settings
+config.default_domain = "WSL:Ubuntu" -- remove if not using WSL
+config.window_decorations = "RESIZE"
+config.window_background_opacity = 0.9
+config.window_padding = {
+	bottom = 0,
+	right = 0,
+}
+config.hide_tab_bar_if_only_one_tab = true
+config.font = wezterm.font_with_fallback({
+	"Monaspace Neon",
+	"Cascadia Code",
+})
+config.harfbuzz_features = { "ss01", "ss02", "ss03", "ss04", "ss05", "ss06", "ss07", "ss08", "calt", "dlig" }
+
 -- keys
 local act = wezterm.action
 
@@ -13,12 +28,5 @@ config.keys = {
 	-- paste from the clipboard
 	{ key = "v", mods = "CTRL", action = act.PasteFrom("Clipboard") },
 }
-
--- terminal appearance
-config.window_decorations = "RESIZE"
-config.window_background_opacity = 0.9
-config.enable_tab_bar = false
-config.font = wezterm.font("MonaspiceNe Nerd Font")
-config.harfbuzz_features = { "ss01", "ss02", "ss03", "ss04", "ss05", "ss06", "ss07", "ss08", "calt", "dlig" }
 
 return config
