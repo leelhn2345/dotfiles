81d352e95cfaf0f06fc2844657e7c9350c3d4fb5
Nelson Lee <leelhn2345@outlook.com>
2024-03-03 22:30:59 +0800

    feat: change to foreground coloring

--- config/nvim/lua/plugins/colorizer.lua
+++ config/nvim/lua/plugins/colorizer.lua
@@ -1,5 +1,9 @@
 return {
-	"NvChad/nvim-colorizer.lua",
-	event = { "BufReadPre", "BufNewFile" },
-	config = true,
+  "NvChad/nvim-colorizer.lua",
+  event = { "BufReadPre", "BufNewFile" },
+  opts = {
+    user_default_options = {
+      mode = "foreground",
+    },
+  },
 }
