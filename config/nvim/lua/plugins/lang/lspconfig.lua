3c7840cac53485b0c716218dd1d6b80242c523ff
Nelson Lee <leelhn2345@outlook.com>
2024-03-22 16:27:51 +0000

    chore: change python type check mode

--- config/nvim/lua/plugins/lang/lspconfig.lua
+++ config/nvim/lua/plugins/lang/lspconfig.lua
@@ -58,7 +58,7 @@ return {
       capabilities = capabilities,
       settings = {
         basedpyright = {
-          typeCheckingMode = "basic",
+          typeCheckingMode = "standard",
         },
       },
     })
