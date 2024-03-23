a8a5b5cbf103438fb67a74d9c1cd39634a9f1065
Nelson Lee <leelhn2345@outlook.com>
2024-03-23 20:52:59 +0800

    refactor: mason

--- config/nvim/lua/plugins/mason.lua
+++ config/nvim/lua/plugins/mason.lua
@@ -1,21 +1,11 @@
 return {
-  "williamboman/mason.nvim",
-  dependencies = {
-    "williamboman/mason-lspconfig.nvim",
-    "WhoIsSethDaniel/mason-tool-installer.nvim",
-  },
-
-  config = function()
-    -- import mason
-    local mason = require("mason")
-
-    -- import mason-lspconfig
-    local mason_lspconfig = require("mason-lspconfig")
-
-    local mason_tool_installer = require("mason-tool-installer")
-
-    -- enable mason and configure icons
-    mason.setup({
+  {
+    "williamboman/mason.nvim",
+    dependencies = {
+      "williamboman/mason-lspconfig.nvim",
+      "WhoIsSethDaniel/mason-tool-installer.nvim",
+    },
+    opts = {
       ui = {
         icons = {
           package_installed = "✓",
@@ -23,10 +13,11 @@ return {
           package_uninstalled = "✗",
         },
       },
-    })
-
-    mason_lspconfig.setup({
-      -- list of servers for mason to install
+    },
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    opts = {
       ensure_installed = {
         "eslint",
         "vtsls",
@@ -42,11 +33,12 @@ return {
         "marksman",
         "bashls",
       },
-      -- auto-install configured servers (with lspconfig)
-      automatic_installation = true, -- not the same as ensure_installed
-    })
-
-    mason_tool_installer.setup({
+      automatic_installation = true,
+    },
+  },
+  {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    opts = {
       ensure_installed = {
         "markdownlint", -- md formatter + linter
         "prettierd", -- prettier linter
@@ -59,6 +51,6 @@ return {
         "hadolint", -- dockerfile linter
         "sql-formatter", -- sql formatter
       },
-    })
-  end,
+    },
+  },
 }
