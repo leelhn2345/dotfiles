8b117aedd69700477ff6d4958cca2c5442e0feba
Nelson Lee <leelhn2345@outlook.com>
2024-05-07 03:43:17 +0800

    feat: ts-context-comment

--- config/nvim/lua/plugins/comment.lua
+++ config/nvim/lua/plugins/comment.lua
@@ -1,11 +1,23 @@
 return {
-  "numToStr/Comment.nvim",
-  config = function()
-    require("Comment").setup()
+  {
+    "JoosepAlviste/nvim-ts-context-commentstring",
+    lazy = true,
+    opts = { enabled_autocmd = false },
+  },
+  {
+    "numToStr/Comment.nvim",
+    dependencies = "JoosepAlviste/nvim-ts-context-commentstring",
+    config = function()
+      require("Comment").setup({
+        pre_hook = require(
+          "ts_context_commentstring.integrations.comment_nvim"
+        ).create_pre_hook(),
+      })
 
-    local opts = { noremap = false, desc = "Toggle Comment" }
+      local opts = { noremap = false, desc = "Toggle Comment" }
 
-    vim.api.nvim_set_keymap("n", "<C-_>", "gcc", opts)
-    vim.api.nvim_set_keymap("v", "<C-_>", "gcc", opts)
-  end,
+      vim.api.nvim_set_keymap("n", "<C-_>", "gcc", opts)
+      vim.api.nvim_set_keymap("v", "<C-_>", "gcc", opts)
+    end,
+  },
 }
