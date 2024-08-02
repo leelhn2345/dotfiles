b4add12c8e2e27f57abd9b5e5d15247e93e2abf2
Nelson Lee <leelhn2345@outlook.com>
2024-08-02 19:41:09 +0800

    refactor: neotree

--- config/nvim/lua/plugins/neo-tree.lua
+++ config/nvim/lua/plugins/neo-tree.lua
@@ -5,39 +5,36 @@ return {
     "nvim-tree/nvim-web-devicons",
     "MunifTanjim/nui.nvim",
   },
-  config = function()
-    require("neo-tree").setup({
-      default_component_configs = {
-        indent = {
-          with_expanders = true,
-        },
-        git_status = {
-          symbols = {
-            ignored = "",
-          },
+  keys = {
+    {
+      "<leader>ee",
+      ":Neotree toggle<CR>",
+      desc = "Toggle File Explorer",
+      silent = true,
+    },
+  },
+  opts = {
+    default_component_configs = {
+      indent = {
+        with_expanders = true,
+      },
+      git_status = {
+        symbols = {
+          ignored = "",
         },
       },
-      filesystem = {
-        hijack_netrw_behavior = "open_current",
-        follow_current_file = {
-          enabled = true,
-        },
-        filtered_items = {
-          hide_dotfiles = false,
-          hide_gitignored = false,
-          -- don't risk messing with git config
-          never_show = { ".git" },
-        },
+    },
+    filesystem = {
+      hijack_netrw_behavior = "open_current",
+      follow_current_file = {
+        enabled = true,
       },
-    })
-
-    local keymap = vim.keymap
-    local opts = { noremap = true, silent = true }
-
-    opts.desc = "Focus File Explorer"
-    keymap.set("n", "<leader>em", ":Neotree focus<CR>", opts)
-
-    opts.desc = "Toggle File Explorer"
-    keymap.set("n", "<leader>ee", ":Neotree toggle<CR>", opts)
-  end,
+      filtered_items = {
+        hide_dotfiles = false,
+        hide_gitignored = false,
+        -- don't risk messing with git config
+        never_show = { ".git" },
+      },
+    },
+  },
 }
