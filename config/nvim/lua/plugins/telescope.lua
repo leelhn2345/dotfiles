0b1de99cb92a75374850c2cf3d1e8bcadb586d05
Nelson Lee <leelhn2345@outlook.com>
2024-08-03 23:57:27 +0800

    chore: enable lsp definition into cargo registry

--- config/nvim/lua/plugins/telescope.lua
+++ config/nvim/lua/plugins/telescope.lua
@@ -23,7 +23,7 @@ local config = function()
         "%.git/",
         ".*%-lock%.json",
         ".*%.lock",
-        "%.cargo/registry/",
+        -- "%.cargo/registry/",
         --   "node_modules/",
         --   "%.venv/",
         --   "dist/",
