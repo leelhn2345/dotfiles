1c3fbaabb76a74cddcc9275d9791f542b08baa00
Nelson Lee <leelhn2345@outlook.com>
2024-08-21 21:40:07 +0800

    refactor: neogen

--- config/nvim/lua/plugins/neogen.lua
+++ config/nvim/lua/plugins/neogen.lua
@@ -1,17 +1,13 @@
 return {
   "danymat/neogen",
-  event = "VeryLazy",
   dependencies = "nvim-treesitter/nvim-treesitter",
-  config = function()
-    require("neogen").setup()
-
-    local opts = { silent = true, desc = "Generate Docstring" }
-
-    vim.api.nvim_set_keymap(
-      "n",
+  keys = {
+    {
       '""',
       ":lua require('neogen').generate()<CR>",
-      opts
-    )
-  end,
+      desc = "Generate Docstring",
+      silent = true,
+    },
+  },
+  opts = {},
 }
