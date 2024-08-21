9232e30dd41196df4f65865f56524a647520457e
Nelson Lee <leelhn2345@outlook.com>
2024-08-21 08:29:55 +0800

    feat: change poetry to uv

--- scripts/coding.sh
+++ scripts/coding.sh
@@ -1,35 +1,35 @@
 #!/usr/bin/env bash
 
 #######################################
-# installs poetry, a python package manager
+# installs uv, a python package manager
 #######################################
-poetry_install() {
-	curl -sSL https://install.python-poetry.org | python3 -
+uv_install() {
+  curl -LsSf https://astral.sh/uv/install.sh | sh
 }
 
 #######################################
 # installs nvm - node version manager
 #######################################
 nvm_install() {
-	curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh | bash
-	# shellcheck disable=SC1090
-	source ~/.nvm/nvm.sh
-	nvm install --lts
+  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh | bash
+  # shellcheck disable=SC1090
+  source ~/.nvm/nvm.sh
+  nvm install --lts
 }
 
 #######################################
 # installs bun, a nodeJS and npm alternative
 #######################################
 bun_install() {
-	curl -fsSL https://bun.sh/install | bash
-	SHELL=/bin/zsh "$HOME/.bun/bin/bun" completions >~/.bun/_bun
+  curl -fsSL https://bun.sh/install | bash
+  SHELL=/bin/zsh "$HOME/.bun/bin/bun" completions >~/.bun/_bun
 }
 
 #######################################
 # installs dotnet
 #######################################
 dotnet_install() {
-	sudo apt-get install -y dotnet-sdk-8.0
+  sudo apt-get install -y dotnet-sdk-8.0
 }
 
 #######################################
@@ -38,16 +38,16 @@ dotnet_install() {
 # uses `goenv` as cmd
 #######################################
 golang_install() {
-	git clone https://github.com/go-nv/goenv.git ~/.goenv
+  git clone https://github.com/go-nv/goenv.git ~/.goenv
 
-	export GOENV_ROOT="$HOME/.goenv"
-	export PATH="$GOENV_ROOT/bin:$PATH"
-	eval "$(goenv init -)"
-	export PATH="$GOROOT/bin:$PATH"
-	export PATH="$PATH:$GOPATH/bin"
+  export GOENV_ROOT="$HOME/.goenv"
+  export PATH="$GOENV_ROOT/bin:$PATH"
+  eval "$(goenv init -)"
+  export PATH="$GOROOT/bin:$PATH"
+  export PATH="$PATH:$GOPATH/bin"
 
-	goenv install latest
-	LATEST_GOLANG_VERSION=$(goenv versions | awk '{print $1}')
-	info "installed latest golang version -- $LATEST_GOLANG_VERSION"
-	goenv global "$LATEST_GOLANG_VERSION"
+  goenv install latest
+  LATEST_GOLANG_VERSION=$(goenv versions | awk '{print $1}')
+  info "installed latest golang version -- $LATEST_GOLANG_VERSION"
+  goenv global "$LATEST_GOLANG_VERSION"
 }
--- scripts/zsh.sh
+++ scripts/zsh.sh
@@ -4,27 +4,27 @@ ZSH_COMPLETIONS_DIR="$HOME/.config/zsh/completions"
 ZSH_PLUGINS_DIR="$HOME/.config/zsh/plugins"
 
 zsh_setup() {
-	chsh -s /bin/zsh
+  chsh -s /bin/zsh
 
-	mkdir -p "$ZSH_COMPLETIONS_DIR"
-	mkdir -p "$ZSH_PLUGINS_DIR"
+  mkdir -p "$ZSH_COMPLETIONS_DIR"
+  mkdir -p "$ZSH_PLUGINS_DIR"
 }
 
 zsh_plugins() {
-	git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "$ZSH_PLUGINS_DIR/powerlevel10k"
-	git clone https://github.com/Aloxaf/fzf-tab "$ZSH_PLUGINS_DIR/fzf-tab"
-	git clone https://github.com/zsh-users/zsh-autosuggestions "$ZSH_PLUGINS_DIR/zsh-autosuggestions"
-	git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$ZSH_PLUGINS_DIR/zsh-syntax-highlighting"
-	git clone https://github.com/zsh-users/zsh-history-substring-search.git "$ZSH_PLUGINS_DIR/zsh-history-substring-search"
-	git clone https://github.com/MichaelAquilina/zsh-you-should-use.git "$ZSH_PLUGINS_DIR/zsh-you-should-use"
+  git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "$ZSH_PLUGINS_DIR/powerlevel10k"
+  git clone https://github.com/Aloxaf/fzf-tab "$ZSH_PLUGINS_DIR/fzf-tab"
+  git clone https://github.com/zsh-users/zsh-autosuggestions "$ZSH_PLUGINS_DIR/zsh-autosuggestions"
+  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$ZSH_PLUGINS_DIR/zsh-syntax-highlighting"
+  git clone https://github.com/zsh-users/zsh-history-substring-search.git "$ZSH_PLUGINS_DIR/zsh-history-substring-search"
+  git clone https://github.com/MichaelAquilina/zsh-you-should-use.git "$ZSH_PLUGINS_DIR/zsh-you-should-use"
 }
 
 zsh_completions() {
-	nala --install-completion zsh >"$ZSH_COMPLETIONS_DIR/_nala"
-	rustup completions zsh cargo >"$ZSH_COMPLETIONS_DIR/_cargo"
-	rustup completions zsh >"$ZSH_COMPLETIONS_DIR/_rustup"
-	"$HOME/.local/bin/poetry" completions zsh >"$ZSH_COMPLETIONS_DIR/_poetry"
-	curl -o "$ZSH_COMPLETIONS_DIR/_golang" https://raw.githubusercontent.com/zsh-users/zsh-completions/master/src/_golang
+  nala --install-completion zsh >"$ZSH_COMPLETIONS_DIR/_nala"
+  rustup completions zsh cargo >"$ZSH_COMPLETIONS_DIR/_cargo"
+  rustup completions zsh >"$ZSH_COMPLETIONS_DIR/_rustup"
+  curl -o "$ZSH_COMPLETIONS_DIR/_golang" https://raw.githubusercontent.com/zsh-users/zsh-completions/master/src/_golang
+  uv generate-shell-completions zsh >"$ZSH_COMPLETIONS_DIR/_uv"
 
-	rm -rf ~/.zfunc
+  rm -rf ~/.zfunc
 }
