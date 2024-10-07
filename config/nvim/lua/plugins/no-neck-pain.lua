acb3d4b124b505d8b6c45fb9d5d2d14e774d47ee
Nelson Lee <leelhn2345@outlook.com>
2024-10-07 22:59:09 +0800

    feat: neck pain

---
+++ config/nvim/lua/plugins/no-neck-pain.lua
@@ -0,0 +1,12 @@
+return {
+  "shortcuts/no-neck-pain.nvim",
+  keys = {
+    {
+      "<leader>np",
+      ":NoNeckPain<CR>",
+      desc = "Toggles neck pain",
+      silent = true,
+    },
+  },
+  cmd = { "NoNeckPain" },
+}
