33a22c4d2971a64bee9dc50bd6601d1639d74437
Nelson Lee <64735315+leelhn2345@users.noreply.github.com>
2024-10-31 18:55:56 +0800

    chore: testing (#5)
    * add comment
    
    * change default merge method
    
    * wowc

--- config/nvim/lua/plugins/git.lua
+++ config/nvim/lua/plugins/git.lua
@@ -45,7 +45,7 @@ return {
       "nvim-tree/nvim-web-devicons",
     },
     opts = {
-      default_merge_method = "squash",
+      default_merge_method = "squash delete",
       enable_builtin = true,
     },
     keys = {
