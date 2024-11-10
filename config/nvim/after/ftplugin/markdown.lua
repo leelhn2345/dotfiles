5626391d13890cbdac3d6e3f75e709b4e54ec9ca
leelhn2345 <leelhn2345@outlook.com>
2024-11-11 02:56:47 +0800

    feat(markdown): override vim runtime options

---
+++ config/nvim/after/ftplugin/markdown.lua
@@ -0,0 +1,6 @@
+-- :verbose set ts sw sts?
+-- have to override vim runtime markdown options
+
+vim.opt_local.tabstop = 2 -- number of spaces per tab
+vim.opt_local.shiftwidth = 2 -- number of spaces for each level of indentation
+vim.opt_local.softtabstop = 2 -- how many spaces is a tab when in insert mode
