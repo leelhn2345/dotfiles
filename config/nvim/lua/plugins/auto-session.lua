c9fb948c5970b14b1fbb1369175f540c383cb9ac
leelhn2345 <leelhn2345@outlook.com>
2024-11-11 02:55:21 +0800

    chore(auto-session): configure session options

--- config/nvim/lua/plugins/auto-session.lua
+++ config/nvim/lua/plugins/auto-session.lua
@@ -3,7 +3,8 @@ return {
   -- can't be VeryLazy
   -- Mason can't be VeryLazy as well due to LSP on 1st file read
   init = function()
-    vim.opt.sessionoptions = "buffers,curdir,terminal,localoptions"
+    vim.o.sessionoptions =
+      "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
   end,
   config = function()
     require("auto-session").setup({
