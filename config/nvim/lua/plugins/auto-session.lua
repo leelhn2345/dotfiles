7a0cdcdaabdbb6a4a673a75fd58e27962053767b
nelsonacsg <nelson@ambercreative.sg>
2024-12-02 16:39:45 +0800

    feat(auto-session)!: auto_create on git repos only

--- config/nvim/lua/plugins/auto-session.lua
+++ config/nvim/lua/plugins/auto-session.lua
@@ -7,12 +7,18 @@ return {
       "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
   end,
   opts = {
-    allowed_dirs = {
-      "~/code/*",
-      -- "~/dotfiles",
-      "~/dotfiles/config/nvim",
-      "/data/*",
-    },
+    -- only automatically create new session files
+    -- inside of git projects
+    auto_create = function()
+      local cmd = "git rev-parse --is-inside-work-tree"
+      return vim.fn.system(cmd) == "true\n"
+    end,
+    -- allowed_dirs = {
+    --   "~/code/*",
+    --   -- "~/dotfiles",
+    --   "~/dotfiles/config/nvim",
+    --   "/data/*",
+    -- },
     bypass_save_file_types = {
       "",
       "no-neck-pain",
