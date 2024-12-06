297e86a99aa26621d6b72746663eb746b503554e
nelsonacsg <nelson@ambercreative.sg>
2024-12-06 14:23:13 +0800

    feat: oil

---
+++ config/nvim/lua/plugins/oil.lua
@@ -0,0 +1,27 @@
+return {
+  "stevearc/oil.nvim",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  lazy = true,
+  cmd = "Oil",
+  keys = {
+    { "-", ":Oil --float<CR>", desc = "Open parent directory", silent = true },
+  },
+  opts = {
+    keymaps = {
+      ["q"] = { "actions.close", mode = "n" },
+    },
+    skip_confirm_for_simple_edits = true,
+    win_options = {
+      wrap = true,
+    },
+    view_options = {
+      show_hidden = true,
+      is_always_hidden = function(name, _)
+        return name == ".git"
+      end,
+    },
+    float = {
+      max_width = 90,
+    },
+  },
+}
