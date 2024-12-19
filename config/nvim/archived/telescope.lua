fe864403511c251444e278ecad5feefd0f7a4e34
nelsonacsg <nelson@ambercreative.sg>
2024-12-19 19:53:08 +0800

    archive telescope

--- config/nvim/archived/telescope.lua
+++ config/nvim/archived/telescope.lua
@@ -1,6 +1,5 @@
 return {
   "nvim-telescope/telescope.nvim",
-  enabled = false,
   dependencies = {
     "nvim-lua/plenary.nvim",
     { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
