d7e2be0afcf7f545a44283ed981cf23fa819ca8e
nelsonacsg <nelson@ambercreative.sg>
2024-12-29 11:47:54 +0800

    feat: yazi nvim

---
+++ config/nvim/lua/plugins/yazi.lua
@@ -0,0 +1,23 @@
+return {
+  "mikavilpas/yazi.nvim",
+  lazy = true,
+  cmd = "Yazi",
+  keys = {
+    {
+      "<leader>-",
+      "<cmd>Yazi<cr>",
+      desc = "Open yazi at the current file",
+    },
+    {
+      "<leader>cw",
+      "<cmd>Yazi cwd<cr>",
+      desc = "Open the file manager in nvim's working directory",
+    },
+  },
+  opts = {
+    open_for_directories = true,
+    keymaps = {
+      show_help = "<f1>",
+    },
+  },
+}
