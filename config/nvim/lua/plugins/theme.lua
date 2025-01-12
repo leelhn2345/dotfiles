dc7042fa263b03de33c497de65e0c765c1e10c93
nelsonacsg <nelson@ambercreative.sg>
2025-01-12 16:56:30 +0800

    refactor(theme): tokyonight config

--- config/nvim/lua/plugins/theme.lua
+++ config/nvim/lua/plugins/theme.lua
@@ -1,42 +1,24 @@
 return {
   "folke/tokyonight.nvim",
+  lazy = false,
   priority = 1000,
-  config = function()
-    require("tokyonight").setup({
-      style = "night",
-      transparent = true,
-      styles = {
-        sidebars = "transparent",
-        floats = "transparent",
-      },
-    })
-    vim.cmd("colorscheme tokyonight-night")
-
-    -- cmp highlight overrides
-    vim.api.nvim_set_hl(0, "Pmenu", { bg = "NONE" })
-
-    -- blink.cmp highlight overrides
-    vim.api.nvim_set_hl(
-      0,
-      "BlinkCmpKindVariable",
-      { bg = "NONE", fg = "#deb887" }
-    )
-    vim.api.nvim_set_hl(
-      0,
-      "BlinkCmpKindInterface",
-      { link = "BlinkCmpKindVariable" }
-    )
-
-    -- nvim-cmp highlight overrides
-    vim.api.nvim_set_hl(
-      0,
-      "CmpItemKindVariable",
-      { bg = "NONE", fg = "#deb887" }
-    )
-    vim.api.nvim_set_hl(
-      0,
-      "CmpItemKindInterface",
-      { link = "CmpItemKindVariable" }
-    )
+  opts = {
+    style = "night",
+    transparent = true,
+    styles = {
+      sidebars = "transparent",
+      floats = "transparent",
+    },
+    on_highlights = function(highlights, _)
+      highlights["Pmenu"] = { bg = "NONE" }
+      highlights["BlinkCmpKindVariable"] = { bg = "NONE", fg = "#deb887" }
+      highlights["BlinkCmpKindInterface"] = { bg = "NONE", fg = "#deb887" }
+      highlights["CmpItemKindVariable"] = { bg = "NONE", fg = "#deb887" }
+      highlights["CmpItemKindInterface"] = { bg = "NONE", fg = "#deb887" }
+    end,
+  },
+  config = function(_, opts)
+    require("tokyonight").setup(opts)
+    vim.cmd("colorscheme tokyonight")
   end,
 }
