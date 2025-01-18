a90e321cc4fa94f19caf686590f686d5886ea1a5
Nelson Lee <leelhn2345@outlook.com>
2025-01-18 15:34:59 +0800

    change to macbook pro m1

--- config/nix/flake.nix
+++ config/nix/flake.nix
@@ -35,8 +35,8 @@
   in
   {
     # Build darwin flake using:
-    # $ darwin-rebuild build --flake .#simple
-    darwinConfigurations."simple" = nix-darwin.lib.darwinSystem {
+    # $ darwin-rebuild build --flake .#MacBook-Pro-M1
+    darwinConfigurations."MacBook-Pro-M1" = nix-darwin.lib.darwinSystem {
       modules = [ configuration ];
     };
   };
