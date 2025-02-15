2e0883d74ce612e5c31bfe6f0074c86e34a36caf
Nelson Lee <leelhn2345@outlook.com>
2025-02-15 11:19:56 +0800

    refactor: wezterm config

--- common/.wezterm.lua
+++ common/.wezterm.lua
@@ -1,11 +1,5 @@
 local wezterm = require("wezterm")
 
-local config = {}
-
-if wezterm.config_builder then
-	config = wezterm.config_builder()
-end
-
 -- change appearance on lost focus
 wezterm.on("update-status", function(window, _)
 	local overrides = window:get_config_overrides() or {}
@@ -17,45 +11,47 @@ wezterm.on("update-status", function(window, _)
 	window:set_config_overrides(overrides)
 end)
 
--- core settings
-config.window_close_confirmation = "NeverPrompt"
--- config.default_domain = "WSL:Ubuntu" -- uncomment if using WSL
-
--- appearance
-config.native_macos_fullscreen_mode = true -- only has an effect when running on macOS
-config.window_decorations = "RESIZE"
-config.window_background_opacity = 0.9
-config.window_padding = {
-	bottom = 0,
-	right = 0,
-}
-config.font_size = 18
-config.hide_tab_bar_if_only_one_tab = true
-config.font = wezterm.font_with_fallback({
-	"MonaspiceNe Nerd Font",
-	"Monaspace Neon",
-	"Cascadia Code",
-})
-config.harfbuzz_features = {
-	"calt",
-	"ss01",
-	"ss02",
-	"ss03",
-	"ss04",
-	"ss05",
-	"ss06",
-	"ss07",
-	"ss08",
-	"ss09",
-	"liga",
-}
-
--- keys
+local config = wezterm.config_builder()
 local act = wezterm.action
 
-config.keys = {
-	-- paste from the clipboard
-	{ key = "v", mods = "CTRL", action = act.PasteFrom("Clipboard") },
+config = {
+	-- core settings
+	window_close_confirmation = "NeverPrompt",
+	-- default_domain = "WSL:Ubuntu" -- uncomment if using WSL
+
+	-- appearance
+	native_macos_fullscreen_mode = true, -- only has an effect when running on macOS
+	window_decorations = "RESIZE",
+	window_background_opacity = 0.9,
+	window_padding = {
+		bottom = 0,
+		right = 0,
+	},
+	font_size = 18,
+	hide_tab_bar_if_only_one_tab = true,
+	font = wezterm.font_with_fallback({
+		"MonaspiceNe Nerd Font",
+		"Monaspace Neon",
+		"Cascadia Code",
+	}),
+	harfbuzz_features = {
+		"calt",
+		"ss01",
+		"ss02",
+		"ss03",
+		"ss04",
+		"ss05",
+		"ss06",
+		"ss07",
+		"ss08",
+		"ss09",
+		"liga",
+	},
+
+	keys = {
+		-- paste from the clipboard
+		{ key = "v", mods = "CTRL", action = act.PasteFrom("Clipboard") },
+	},
 }
 
 return config
