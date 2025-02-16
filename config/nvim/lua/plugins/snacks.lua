fed527dc891c1147903be0f28a04a0a56a989646
Nelson Lee <leelhn2345@outlook.com>
2025-02-16 23:07:52 +0800

    snacks nvim

---
+++ config/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,15 @@
+return {
+  "folke/snacks.nvim",
+  priority = 1000,
+  lazy = false,
+  opts = {
+    styles = {
+      input = {
+        relative = "cursor",
+      },
+    },
+    bigfile = { enabled = true },
+    indent = { enabled = true },
+    input = { enabled = true },
+  },
+}
