90981e8992ecddbec483fe90cf1e977759970fc5
Nelson Lee <leelhn2345@outlook.com>
2025-02-16 21:51:53 +0800

    fix: load inline diagnostic before rustaceanvim

--- config/nvim/lua/plugins/code/rustaceanvim.lua
+++ config/nvim/lua/plugins/code/rustaceanvim.lua
@@ -2,6 +2,9 @@ return {
   "mrcjkb/rustaceanvim",
   version = "^5", -- Recommended
   ft = { "rust" },
+  dependencies = {
+    "rachartier/tiny-inline-diagnostic.nvim", -- load 1st for inline diagnostics
+  },
   opts = {
     tools = {
       float_win_config = {
