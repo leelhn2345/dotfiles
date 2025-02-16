ad40a9745847dede0431c5c931b1304d5e7a1983
Nelson Lee <leelhn2345@outlook.com>
2025-02-16 23:34:33 +0800

    refactor: snacks nvim

--- config/nvim/lua/plugins/snacks.lua
+++ config/nvim/lua/plugins/snacks.lua
@@ -2,14 +2,18 @@ return {
   "folke/snacks.nvim",
   priority = 1000,
   lazy = false,
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+  },
   opts = {
+    bigfile = { enabled = true },
+    indent = { enabled = true },
+    input = { enabled = true },
+
     styles = {
       input = {
         relative = "cursor",
       },
     },
-    bigfile = { enabled = true },
-    indent = { enabled = true },
-    input = { enabled = true },
   },
 }
