672681b01d3caa14e4b4156a4015a178c495ff82
Nelson Lee <leelhn2345@outlook.com>
2025-03-27 16:45:20 +0800

    refactor: inline-diagnostics

--- config/nvim/lua/plugins/inline-diagnostic.lua
+++ config/nvim/lua/plugins/inline-diagnostic.lua
@@ -1,19 +1,20 @@
 return {
   "rachartier/tiny-inline-diagnostic.nvim",
-  event = "LspAttach",
-  config = function()
-    vim.diagnostic.config({ virtual_text = false })
-
-    require("tiny-inline-diagnostic").setup({
-      options = {
-        -- Enable diagnostic message on all lines.
-        multilines = {
-          enabled = true,
-          always_show = true,
-        },
-        -- Show all diagnostics on the cursor line.
-        show_all_diags_on_cursorline = true,
+  event = "VeryLazy",
+  priority = 1000,
+  opts = {
+    options = {
+      -- Enable diagnostic message on all lines.
+      multilines = {
+        enabled = true,
+        always_show = true,
       },
-    })
+      -- Show all diagnostics on the cursor line.
+      show_all_diags_on_cursorline = true,
+    },
+  },
+  config = function(_, opts)
+    vim.diagnostic.config({ virtual_text = false })
+    require("tiny-inline-diagnostic").setup(opts)
   end,
 }
