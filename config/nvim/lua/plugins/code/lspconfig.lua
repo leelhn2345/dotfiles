be298c298cfc7f214da858171d56bb153ebc53ce
Nelson Lee <leelhn2345@outlook.com>
2025-04-22 18:00:20 +0800

    modify lspconfig

--- config/nvim/lua/plugins/code/lspconfig.lua
+++ config/nvim/lua/plugins/code/lspconfig.lua
@@ -10,7 +10,8 @@ return {
       capabilities = {
         textDocument = {
           -- Tell the server the capability of foldingRange,
-          -- Neovim hasn't added foldingRange to default capabilities, users must add it manually
+          -- Neovim hasn't added foldingRange to default capabilities,
+          -- users must add it manually
           foldingRange = {
             dynamicRegistration = false,
             lineFoldingOnly = true,
@@ -31,30 +32,5 @@ return {
     end
 
     vim.lsp.enable(servers)
-
-    -- configure csharp server
-    -- lspconfig["omnisharp"].setup({
-    --   capabilities = capabilities,
-    --   on_attach = function(_, bufnr)
-    --     local bufopts = {
-    --       desc = "Show LSP definition",
-    --       noremap = true,
-    --       silent = true,
-    --       buffer = bufnr,
-    --     }
-    --     vim.keymap.set("n", "gd", function()
-    --       require("omnisharp_extended").lsp_definitions()
-    --     end, bufopts)
-    --   end,
-    --   cmd = {
-    --     vim.fn.expand("~/.local/share/nvim/mason/bin/omnisharp"),
-    --     "--languageserver",
-    --     "--hostPID",
-    --     tostring(vim.fn.getpid()),
-    --   },
-    --   handlers = {
-    --     ["textDocument/definition"] = require("omnisharp_extended").handler,
-    --   },
-    -- })
   end,
 }
