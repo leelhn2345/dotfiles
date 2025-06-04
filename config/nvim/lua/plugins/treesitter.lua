29fd7765cdfb2f82c633faefc5cca898c8c4a44f
Nelson Lee <leelhn2345@outlook.com>
2025-06-04 22:22:06 +0800

    revert: use treesitter's master branch change because nvim-bqf is not compatible with treesitter's main branch yet.

--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -1,37 +1,50 @@
 return {
   "nvim-treesitter/nvim-treesitter",
-  lazy = false,
-  branch = "main",
+  event = { "BufReadPre", "BufNewFile" },
   build = ":TSUpdate",
-  init = function()
-    vim.api.nvim_create_autocmd("FileType", {
-      callback = function(args)
-        local filetype = args.match
-
-        local excluded_filetypes = {
-          "dockerfile",
-          "tmux",
-        }
-        if vim.tbl_contains(excluded_filetypes, filetype) then
-          return
-        end
-
-        local lang = vim.treesitter.language.get_lang(filetype)
-        local ts = require("nvim-treesitter")
-        local ts_config = require("nvim-treesitter.config")
-
-        if not vim.tbl_contains(ts_config.get_available(), lang) then
-          return
-        end
-
-        if vim.tbl_contains(ts_config.get_installed(), lang) then
-          vim.treesitter.start()
-        else
-          ts.install(lang):await(function()
-            vim.treesitter.start()
-          end)
-        end
-      end,
-    })
+  opts = {
+    auto_install = true,
+    ignore_install = { "dockerfile", "tmux" },
+    highlight = {
+      enable = true,
+    },
+  },
+  config = function(_, opts)
+    require("nvim-treesitter.configs").setup(opts)
   end,
+  -- "nvim-treesitter/nvim-treesitter",
+  -- lazy = false,
+  -- branch = "main",
+  -- build = ":TSUpdate",
+  -- init = function()
+  --   vim.api.nvim_create_autocmd("FileType", {
+  --     callback = function(args)
+  --       local filetype = args.match
+  --
+  --       local excluded_filetypes = {
+  --         "dockerfile",
+  --         "tmux",
+  --       }
+  --       if vim.tbl_contains(excluded_filetypes, filetype) then
+  --         return
+  --       end
+  --
+  --       local lang = vim.treesitter.language.get_lang(filetype)
+  --       local ts = require("nvim-treesitter")
+  --       local ts_config = require("nvim-treesitter.config")
+  --
+  --       if not vim.tbl_contains(ts_config.get_available(), lang) then
+  --         return
+  --       end
+  --
+  --       if vim.tbl_contains(ts_config.get_installed(), lang) then
+  --         vim.treesitter.start()
+  --       else
+  --         ts.install(lang):await(function()
+  --           vim.treesitter.start()
+  --         end)
+  --       end
+  --     end,
+  --   })
+  -- end,
 }
