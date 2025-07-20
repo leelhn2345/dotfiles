7eb753923ddeceffaff5f2350c0fbe7ba145503b
Nelson Lee <leelhn2345@outlook.com>
2025-07-20 12:53:42 +0800

    feat(nvim): disable origami autoFold

--- config/nvim/lua/plugins/origami.lua
+++ config/nvim/lua/plugins/origami.lua
@@ -11,14 +11,12 @@ return {
   end,
   opts = {
     foldtext = {
-      enabled = true,
-      padding = 3,
       lineCount = {
         template = "󰁂 %d lines", -- `%d` is replaced with the number of folded lines
-        hlgroup = "Comment",
       },
-      diagnosticsCount = true, -- uses hlgroups and icons from `vim.diagnostic.config().signs`
-      gitsignsCount = true, -- requires `gitsigns.nvim`
+    },
+    autoFold = {
+      enabled = false,
     },
   },
 }
