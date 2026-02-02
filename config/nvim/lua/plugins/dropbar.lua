d6815f87bfe94e12987abfa898d09058ebc3d66e
Nelson Lee <leelhn2345@outlook.com>
2026-02-02 15:02:08 +0800

    style(nvim): drop dir icon in dropbar

--- config/nvim/lua/plugins/dropbar.lua
+++ config/nvim/lua/plugins/dropbar.lua
@@ -4,5 +4,15 @@ return {
     symbol = {
       on_click = false,
     },
+    icons = {
+      kinds = {
+        dir_icon = "",
+      },
+    },
+    -- sources = {
+    --   path = {
+    --     max_depth = 4,
+    --   },
+    -- },
   },
 }
