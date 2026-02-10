2dcbb21c853a2135a70c2efb150caa4876630c39
Nelson Lee <leelhn2345@outlook.com>
2026-02-10 23:22:01 +0800

    refactor(nvim): nvim-jdtls

--- config/nvim/lua/plugins/code/nvim-jdtls.lua
+++ config/nvim/lua/plugins/code/nvim-jdtls.lua
@@ -1,4 +1,22 @@
 return {
   "mfussenegger/nvim-jdtls",
   ft = { "java" },
+  opts = {
+    name = "jdtls",
+    cmd = { "jdtls" },
+    settings = {
+      java = {},
+    },
+    init_options = {
+      bundles = {},
+    },
+  },
+  config = function(_, opts)
+    local config = vim.tbl_extend(
+      "force",
+      opts,
+      { root_dir = vim.fs.root(0, vim.lsp.config.jdtls.root_markers) }
+    )
+    require("jdtls").start_or_attach(config)
+  end,
 }
--- config/nvim/lua/plugins/mason.lua
+++ config/nvim/lua/plugins/mason.lua
@@ -25,6 +25,7 @@ return {
           "ty",
           --
           "yamlls", -- yaml-companion,
+          "jdtls",
         },
       },
     },
