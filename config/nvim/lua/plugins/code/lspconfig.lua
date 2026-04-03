d835925318eb0f0baa7ad72768e68e8c6cba25b7
Nelson Lee <leelhn2345@outlook.com>
2026-04-03 02:00:11 +0000

    feat(nvim): lsp commands

--- config/nvim/lua/plugins/code/lspconfig.lua
+++ config/nvim/lua/plugins/code/lspconfig.lua
@@ -8,5 +8,18 @@ return {
     vim.api.nvim_create_user_command("LspInfo", function()
       vim.cmd("checkhealth vim.lsp")
     end, { desc = "LSP Information" })
+
+    vim.api.nvim_create_user_command("LspRestart", "lsp restart", {
+      desc = "Restart LSP",
+    })
+
+    vim.api.nvim_create_user_command("LspLog", function(_)
+      local state_path = vim.fn.stdpath("state")
+      local log_path = vim.fs.joinpath(state_path, "lsp.log")
+
+      vim.cmd(string.format("tabedit %s", log_path))
+    end, {
+      desc = "Show LSP log",
+    })
   end,
 }
