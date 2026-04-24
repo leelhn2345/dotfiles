f1c18bd6d69747c11d768d4880929f3d6d29cf99
Nelson Lee <leelhn2345@outlook.com>
2026-04-24 17:50:12 +0800

    refactor(ubuntu): golang installation script

--- scripts/golang.sh
+++ scripts/golang.sh
@@ -2,14 +2,14 @@
 # installs golang-go
 #######################################
 golang_install() {
-  GO_VERSION=1.26.2
+  GO_VERSION=1.26.2.linux-amd64
 
-  curl -OL https://go.dev/dl/go${GO_VERSION}.linux-amd64.tar.gz
+  curl -OL https://go.dev/dl/go${GO_VERSION}.tar.gz
 
   sudo rm -rf /usr/local/go
-  sudo tar -C /usr/local -xzf go${GO_VERSION}.linux-amd64.tar.gz
+  sudo tar -C /usr/local -xzf go${GO_VERSION}.tar.gz
 
-  rm go${GO_VERSION}.linux-amd64.tar.gz
+  rm go${GO_VERSION}.tar.gz
 }
 
 golang_tools() {
