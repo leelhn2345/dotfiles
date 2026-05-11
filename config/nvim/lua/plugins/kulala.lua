4106eaf22e15924e3fe2865b4a1615678fb305bd
Nelson Lee <leelhn2345@outlook.com>
2026-05-11 09:26:33 +0800

    chore(nvim): tag kulala to develop branch main branch doesn't have treesitter fixes yet.

--- config/nvim/lua/plugins/kulala.lua
+++ config/nvim/lua/plugins/kulala.lua
@@ -1,10 +1,9 @@
 return {
   "mistweaverco/kulala.nvim",
   ft = { "http" },
+  -- NOTE: main branch doesnt have treesitter fixes yet.
+  branch = "develop",
   opts = {
     global_keymaps = true,
-    ui = {
-      display_mode = "float",
-    },
   },
 }
