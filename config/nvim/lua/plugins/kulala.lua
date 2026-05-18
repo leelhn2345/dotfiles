5e5579a8224a8193c4cf2d1f5d1096ab2720a515
Nelson Lee <leelhn2345@outlook.com>
2026-05-18 12:20:36 +0800

    feat(nvim): kulala prefix

--- config/nvim/lua/plugins/kulala.lua
+++ config/nvim/lua/plugins/kulala.lua
@@ -1,9 +1,18 @@
+local prefix = "<leader>K"
+
 return {
   "mistweaverco/kulala.nvim",
   ft = { "http" },
+
   -- NOTE: main branch doesnt have treesitter fixes yet.
   branch = "develop",
+
+  keys = {
+    { prefix, "", desc = "+Kulala", mode = { "n", "v" } },
+  },
+
   opts = {
     global_keymaps = true,
+    global_keymaps_prefix = prefix,
   },
 }
