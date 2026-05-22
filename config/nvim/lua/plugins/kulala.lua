5a274bb7aa7c5a4d43af82a5f317c10635fc195d
Nelson Lee <leelhn2345@outlook.com>
2026-05-22 09:30:22 +0800

    feat(nvim): update kulala to v6.0

--- config/nvim/lua/plugins/kulala.lua
+++ config/nvim/lua/plugins/kulala.lua
@@ -3,15 +3,11 @@ local prefix = "<leader>K"
 return {
   "mistweaverco/kulala.nvim",
   ft = { "http" },
-
-  -- NOTE: main branch doesnt have treesitter fixes yet.
-  branch = "develop",
-
   keys = {
     { prefix, "", desc = "+Kulala", mode = { "n", "v" } },
   },
-
   opts = {
+    default_env = "dev",
     global_keymaps = true,
     global_keymaps_prefix = prefix,
   },
