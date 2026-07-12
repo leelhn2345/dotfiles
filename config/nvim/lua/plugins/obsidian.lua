4c4e8f92612b3f6a09c0b71b2d1c081f0197608b
Nelson Lee <leelhn2345@outlook.com>
2026-07-12 14:31:25 +0800

    chore(nvim): new obsidian hotkeys + deps

--- config/nvim/lua/plugins/obsidian.lua
+++ config/nvim/lua/plugins/obsidian.lua
@@ -1,6 +1,7 @@
 return {
   "obsidian-nvim/obsidian.nvim",
   version = "*", -- use latest release, remove to use latest commit
+  dependencies = { "ibhagwan/fzf-lua" },
   event = {
     "BufReadPre " .. vim.fn.expand("~") .. "/vault*/**.md",
     "BufNewFile " .. vim.fn.expand("~") .. "/vault*/**.md",
@@ -22,13 +23,29 @@ return {
     },
     { "<leader>on", "<CMD>Obsidian new<CR>", desc = "Obsidian new note" },
     { "<leader>od", "<CMD>Obsidian dailies<CR>", desc = "Obsidian dailies" },
+    {
+      "<leader>ob",
+      "<CMD>Obsidian backlinks<CR>",
+      desc = "Obsidian backlinks",
+    },
+    {
+      "<leader>on",
+      ":Obsidian extract_note<CR>",
+      desc = "Obsidian extract note",
+      mode = { "x" },
+    },
+    {
+      "<leader>oc",
+      ":Obsidian toggle_checkbox<CR>",
+      desc = "Obsidian toggle checkbox",
+      mode = { "n", "x" },
+    },
   },
   opts = function()
     return {
       legacy_commands = false, -- this will be removed in 4.0.0
       note_id_func = require("obsidian.builtin").title_id,
       workspaces = {
-
         {
           -- docs: https://obsidian.md/help/sync/headless
           -- run till `ob sync`
@@ -54,6 +71,8 @@ return {
       sync = {
         enabled = true,
       },
+      -- footer = { enabled = false }, -- reduce rg calls, which are slow for large vaults
+      -- search = { sort = false }, -- avoid rg sorting, which is slow for large vaults
       frontmatter = {
         func = function(note)
           local out = {
